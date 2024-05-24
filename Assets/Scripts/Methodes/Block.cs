c3782cb1f94c6bbb070da416f89aba8629bed3bc
DrayLiix3wa <remi.deslandes@3wa.io>
2024-05-24 14:50:51 +0200

    feat(Dash/ Hit / InputSystem / CameraFollow / CameraAnchor) : Ajout de Dash/ Hit / InputSystem / CameraFollow / CameraAnchor

---
+++ Assets/Scripts/Methodes/Block.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Dirk Dynamite/Block")]
+[DisallowMultipleComponent]
+
+public class Block : MonoBehaviour
+{
+    public bool isBlocking;
+    public float blockSpeed;
+
+    public void OnBlockEnter()
+    {
+
+    }
+
+    public void OnBlockPerform()
+    {
+
+    }
+
+    public void OnBlockExit()
+    {
+
+    }
+}
