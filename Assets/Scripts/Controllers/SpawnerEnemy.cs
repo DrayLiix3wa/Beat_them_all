e88ba10be2247b4c858555486851beef11a98624
Kiramuun <deimonlight@live.fr>
2024-05-27 15:21:32 +0200

    Update SpawnerEnemy.cs

--- Assets/Scripts/Controllers/SpawnerEnemy.cs
+++ Assets/Scripts/Controllers/SpawnerEnemy.cs
@@ -13,26 +13,37 @@ public class Wave
 public class SpawnerEnemy : MonoBehaviour
 {
     public Wave[] myWaves;
-    int waveCount;
+    [SerializeField] Transform _grpEnemy;
+    int waveCount = 0;
+    bool isRunning = true;
 
     void Start()
     {
-        // Je spawn la premiere vague du tableau
-        StartCoroutine(Spawn(waveCount));
+        if (isRunning == true)
+        {
+            StartCoroutine(Spawn(waveCount));
+        }
     }
 
     void Update()
     {
-
+        if(_grpEnemy.childCount == 0)
+        {
+            waveCount++;
+            isRunning = true;
+            Start();
+        }
     }
 
     IEnumerator Spawn(int waveID)
     {
         foreach (var e in myWaves[waveID]._enemies)
         {
+            isRunning = true;
             Vector2 spawnPos = (Vector2)transform.position;
             Instantiate(e, spawnPos, Quaternion.identity);
             yield return new WaitForSeconds(myWaves[waveID].spawnInterval);
+            isRunning = false;
         }
 
     }
