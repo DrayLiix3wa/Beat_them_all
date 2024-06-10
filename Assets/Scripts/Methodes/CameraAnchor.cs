7e2b16277db15a0dbe69956d166a6b479c148341
DrayLiix3wa <remi.deslandes@3wa.io>
2024-06-10 15:03:16 +0200

    feat(farwest) :  Modification Playlist Horror et camera follow

--- Assets/Scripts/Methodes/CameraAnchor.cs
+++ Assets/Scripts/Methodes/CameraAnchor.cs
@@ -13,8 +13,8 @@ public class CameraAnchor : MonoBehaviour
     {
         Vector2 desiredPosition = playerBody.position + playerBody.velocity;
 
-        desiredPosition.y = Mathf.Clamp(desiredPosition.y, playerBody.position.y - 0.5f, playerBody.position.y + 0.8f);
-        desiredPosition.x = Mathf.Clamp(desiredPosition.x, playerBody.position.x - 1.5f, playerBody.position.x + 1.5f);
+        desiredPosition.y = Mathf.Clamp(desiredPosition.y, playerBody.position.y - 0.3f, playerBody.position.y + 0.3f);
+        desiredPosition.x = Mathf.Clamp(desiredPosition.x, playerBody.position.x - 0.5f, playerBody.position.x + 0.5f);
 
         transform.position = Vector3.Lerp(transform.position, desiredPosition, Mathf.SmoothStep(0f, 1f, 0.1f));
     }
